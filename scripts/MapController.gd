03f3ffef52c441803c135c7933ab82db85983642
Shaun <32381182+ShaunJae@users.noreply.github.com>
2023-10-02 14:24:52 -0400

    Add files via upload

--- scripts/MapController.gd
+++ scripts/MapController.gd
@@ -3,24 +3,89 @@ extends TileMap
 #[====================   RUNNING LOGIC   ====================]
 
 var time : float = 0
-const MAX_TILES : int = 500
+var MAX_TILES : int = 200
+
+var dungeon_location : Sprite 
+var dungeon_velocity : Vector2 = Vector2(rand_range(-1,1), rand_range(-1,1)).normalized() * 4
+var dungeon_redirect : float = 0
 
 func _physics_process(delta):
 	time += delta
-	if time >= 1.0:
-#		print("Dungeon change tick")
+	if time >= 0.2:
+		#Collapse a part of the dungeon
 		if randf() < generation_speed():
-#			print(" Changing...")
-			var random_floor = active_floors.keys()[randi() % active_floors.keys().size()]
-			var random_wall = active_walls.keys()[randi() % active_walls.keys().size()]
-			change_cell_data(random_floor, wall_tile_index)
+			var random_wall : Vector2 = pick_tile_close_to_dungeon(true) #active_walls.keys()[randi() % active_walls.keys().size()]
 			change_cell_data(random_wall, floor_tile_index)
+		if randf() > generation_speed():
+			var random_floor : Vector2 = pick_tile_close_to_dungeon(false) #active_floors.keys()[randi() % active_floors.keys().size()]
+			var new_particles = load("res://scenes/CollapseParticles.tscn").instance()
+			self.add_child(new_particles)
+			new_particles.emitting = true
+			new_particles.position = map_to_world(random_floor)
+			change_cell_data(random_floor, wall_tile_index)
+		
+		#Remove floating walls
+		for wall_tile in active_walls.keys():
+			if active_walls[wall_tile]["neighbors"] == 1:
+				change_cell_data(wall_tile, floor_tile_index)
+		
+		#Remove solo floors
+		for floor_tile in active_floors.keys():
+			if active_floors[floor_tile]["neighbors"] == 0:
+				change_cell_data(floor_tile, wall_tile_index)
+		
+		#Update bounding boxes for projectiles
+		update_bounding_boxes()
+		
 		time = 0
+	
+	#Let the dungeon location meander
+	dungeon_redirect += delta
+	if dungeon_redirect >= 5:
+		#print(dungeon_velocity)
+		var modifier_velocity : Vector2 = Vector2(rand_range(-1,1), rand_range(-1,1)).normalized() * 2
+		dungeon_velocity = (dungeon_velocity + modifier_velocity).normalized() * 4
+		dungeon_redirect = 0
+	
+	dungeon_location.position += dungeon_velocity * delta
 
 #https://www.desmos.com/calculator/5hkvetx2vt
 func generation_speed(max_tiles : int = MAX_TILES, floor_size : int = current_floor_size) -> float:
+	if floor_size < 0:
+		return float(1)
+	if floor_size > max_tiles:
+		return float(0)
 	return sqrt(-1 * ((float(floor_size) - max_tiles) / max_tiles))
+
+#Selects a random tile from the respective active dictionary 
+func pick_tile_close_to_dungeon(minimize : bool) -> Vector2:
+	#Find tile type to be changed from and setup to find a random tile in that dictionary
+	var tile_type : int = wall_tile_index if minimize else floor_tile_index
+	var respective_dictionary = get_respective_dictionary(tile_type)
+	var random_selection_count : int = clamp(respective_dictionary.size() / 8, 1, INF)
+	var current_tile : Vector2 = Vector2(0, 0)
+	var current_distance : float = INF if minimize else 0
 	
+	#itterate through an amount of tiles and pick one that is closest to the room
+	for i in random_selection_count:
+		var random_tile = respective_dictionary.keys()[randi() % respective_dictionary.keys().size()]
+		var distance : float = dungeon_location.position.distance_to(map_to_world(random_tile))
+		if (minimize and distance < current_distance) or (!minimize and distance > current_distance):
+			current_distance = distance
+			current_tile = random_tile
+	
+	return current_tile
+
+onready var top_left : Position2D = $"../TopLeft"
+onready var bottom_right : Position2D = $"../BottomRight"
+const SCALE : int = 64
+
+func update_bounding_boxes():
+	var tile_rect = self.get_used_rect()
+	top_left.position = tile_rect.position * SCALE
+	bottom_right.position = tile_rect.end * SCALE
+	$"..".update_bounding_rects()
+
 #[====================  TILE MANAGEMENT  ====================]
 
 #Indexes for respective tiles in the attatched Tileset
@@ -42,8 +107,15 @@ const base_tile_dict = {
 const neighbor_mask = [Vector2(-1, 0), Vector2(0, 1), Vector2(1, 0), Vector2(0, -1)]
 
 func _ready():
+	create_dungeon_location()
 	initial_check()
-	print(current_floor_size)
+
+func create_dungeon_location():
+	dungeon_location = Sprite.new()
+	dungeon_location.position = Vector2(136, 80)
+	dungeon_location.texture = load("res://icon.png")
+	dungeon_location.scale = Vector2(0.125, 0.125)
+	self.add_child(dungeon_location)
 
 func change_cell_data(cell : Vector2, new_tile_index : int) -> void:
 	
@@ -58,6 +130,11 @@ func change_cell_data(cell : Vector2, new_tile_index : int) -> void:
 		#Add cell to dictionary and update neighbors
 		add_cell_to_dictionary(cell)
 		update_neighbors(cell)
+	
+	#If an empty space is being turned to wall
+	if old_cell_tile_index == -1:
+		change_cell_on_tilemap(cell, new_tile_index)
+		add_cell_to_dictionary(cell)
 
 #Adds cell to respective dictionary
 func add_cell_to_dictionary(cell : Vector2, equivalent_neighbors : int = -1, cell_tile_index : int = -2) -> void:
@@ -77,8 +154,12 @@ func add_cell_to_dictionary(cell : Vector2, equivalent_neighbors : int = -1, cel
 func change_cell_on_tilemap(cell : Vector2, tile_index : int) -> void:
 	
 	#Check if a floor tile is being removed and update floor size
-	if self.get_cellv(cell) == floor_tile_index and tile_index != floor_tile_index:
+	if self.get_cellv(cell) == floor_tile_index and tile_index == wall_tile_index:
 		current_floor_size -= 1
+		
+	#Check if a wall tile is being removed and update floor size
+	if self.get_cellv(cell) == wall_tile_index and tile_index == floor_tile_index:
+		current_floor_size += 1
 	
 	#Update tilemap
 	self.set_cellv(cell, tile_index)
@@ -112,7 +193,7 @@ func get_equivalent_neighbors(cell : Vector2) -> int:
 	var equivalent_neighbors : int = 0
 	for neighbor in neighbor_mask:
 		var neighbor_tile_index = self.get_cellv(cell + neighbor)
-		if cell_tile_index == neighbor_tile_index or neighbor_tile_index == -1:
+		if cell_tile_index == neighbor_tile_index or (neighbor_tile_index == -1 and cell_tile_index == wall_tile_index):
 			equivalent_neighbors += 1
 	return equivalent_neighbors
 
@@ -120,15 +201,26 @@ func get_equivalent_neighbors(cell : Vector2) -> int:
 func update_neighbors(cell : Vector2) -> void:
 	for neighbor in neighbor_mask:
 		
-		#Collect cell information
+		#Collect cell information and make into a wall if unused
 		var current_cell = cell + neighbor
 		var neighbor_tile_index : int = self.get_cellv(current_cell)
+		
+		#If the neighboring tile is empty and we're updating around a floor, make that empty a wall
+		if neighbor_tile_index == -1 and self.get_cellv(cell) == floor_tile_index:
+			change_cell_data(current_cell, wall_tile_index)
+			neighbor_tile_index = wall_tile_index
+		
 		var equivalent_neighbors = get_equivalent_neighbors(current_cell)
 		var respective_dictionary = get_respective_dictionary(neighbor_tile_index)
 		
-		#If 4 equivalent neighbors, inert cell, remove if previously active, remove from dictionary and continute
+		#If 4 equivalent neighbors, inert cell, continue 
 		if equivalent_neighbors == 4:
-			respective_dictionary.erase(current_cell)
+			#Remove from dictionary if previoisly active
+			if respective_dictionary.keys().has(current_cell):
+				respective_dictionary.erase(current_cell)
+			#If wall, change back to empty 
+			if neighbor_tile_index == wall_tile_index:
+				change_cell_on_tilemap(current_cell, -1)
 			continue
 		#Otherwise, check for pre-existing value, add or update respectivly
 		else:
--- scripts/Powerup.gd
+++ scripts/Powerup.gd
@@ -1,17 +1,20 @@
 extends Area2D
 
-const effects = ["BIG", "SHOTGUN", "DOUBLE"]
+var strength = 0
+const effects = [" BIG", "SHOTGUN", "DOUBLE"]
 export (String, "BIG", "SHOTGUN", "DOUBLE") var effect
 
 
 func _ready():
 	var rand = randi() % len(effects)
+	strength = randi() % 2
 	effect = effects[rand]
 	$Sprite.texture = $Sprite.texture.duplicate(true)
 	$Sprite.texture.region.position.x = 80 + rand * 16
+	$Sprite.texture.region.position.y = 144 if strength == 1 else 160
 
 
 func _on_Powerup_body_entered(body):
 	if body.has_method('powerup'):
-		body.powerup(effect)
+		body.powerup(effect, strength)
 		queue_free()
